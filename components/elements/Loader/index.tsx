b82caaaec88abee86458c27a5f0d4ef3399e362f
David <david-bourrel@hotmail.fr>
2022-06-07 21:15:54 +0200

    loader fixed

--- components/elements/Loader/index.tsx
+++ components/elements/Loader/index.tsx
@@ -4,9 +4,16 @@ import styles from './Loader.module.css';
 interface LoaderProps {
   svgClassName?: string;
   circleClassName?: string;
+  width?: string;
+  height?: string;
 }
 
-const Loader: FC<LoaderProps> = ({ svgClassName, circleClassName }) => {
+const Loader: FC<LoaderProps> = ({
+  svgClassName,
+  circleClassName,
+  width = '30px',
+  height = '30px',
+}) => {
   const computedSvgClassName = useMemo(
     () => (svgClassName ? svgClassName : styles.loader),
     [svgClassName]
@@ -19,11 +26,20 @@ const Loader: FC<LoaderProps> = ({ svgClassName, circleClassName }) => {
   return (
     <svg
       className={computedSvgClassName}
-      viewBox="0 0 60 60"
-      fill="none"
+      width={width}
+      height={height}
+      viewBox="0 0 66 66"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle className={computedCircleClassName} cx="30" cy="30" r="27" />
+      <circle
+        className={computedCircleClassName}
+        fill="none"
+        strokeWidth="6"
+        strokeLinecap="round"
+        cx="33"
+        cy="33"
+        r="30"
+      />
     </svg>
   );
 };
