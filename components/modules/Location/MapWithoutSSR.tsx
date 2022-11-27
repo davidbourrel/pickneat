60cf63d4819e80229dbbd67691ee8d94949c195c
David <david-bourrel@hotmail.fr>
2022-11-27 13:23:35 +0100

    dot env changed

--- components/modules/Location/MapWithoutSSR.tsx
+++ components/modules/Location/MapWithoutSSR.tsx
@@ -7,7 +7,7 @@ import styles from './Location.module.css';
 
 const MapWithoutSSR = () => {
   const position: [number, number] = [43.59990664483555, 1.442283655554873];
-  const mapUrl = `${process.env.NEXT_PUBLIC_MAPBOX_URL}${process.env.NEXT_PUBLIC_MAPBOX_KEY}`;
+  const mapUrl = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_KEY}`;
 
   return (
     <MapContainer
