b28c639686d9b420431f94dceaf74c2c3549a07d
David <david-bourrel@hotmail.fr>
2023-01-01 15:31:45 +0100

    user page little improved

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,14 +1,14 @@
 import { useMemo } from 'react';
-import { useTranslations } from 'next-intl';
-import Head from 'next/head';
 import type { GetServerSidePropsContext } from 'next';
-import { getSession, UserProfile } from '@auth0/nextjs-auth0';
-import Link from 'components/elements/Link';
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import Head from 'next/head';
 import styles from './Profile.module.css';
+import { getSession, UserProfile } from '@auth0/nextjs-auth0';
+import { pick } from 'lodash';
+import Link from 'components/elements/Link';
 import Button from 'components/elements/buttons/Button';
 import Heading from 'components/elements/Heading';
-import Image from 'next/image';
-import { pick } from 'lodash';
 import PageLayout from 'components/modules/PageLayout';
 import MainContentLayout from 'components/modules/MainContentLayout';
 
@@ -17,41 +17,65 @@ interface ProfileProps {
 }
 
 export default function Profile({ user }: ProfileProps) {
-  const t = useTranslations('Navigation');
+  const { nickname, email, picture } = user;
+
+  const t = useTranslations('Profile');
+  const t2 = useTranslations('Navigation');
 
   const userPicture = useMemo(
     () =>
-      user && user.picture ? (
+      picture ? (
         <Image
-          src={user.picture}
+          src={picture}
           alt="User profile"
           width={400}
           height={400}
           className={styles.profilePicture}
         />
       ) : null,
-    [user]
+    [picture]
+  );
+
+  const userInformation = useMemo(
+    () =>
+      nickname && email ? (
+        <div className={styles.userInformation}>
+          <p>
+            <span>{t('nickname')} :</span> {nickname}
+          </p>
+          <p>
+            <span>{t('email')} :</span> {email}
+          </p>
+        </div>
+      ) : null,
+    [nickname, email, t]
   );
 
   return (
     <MainContentLayout>
       <Head>
-        <title>{`PickN\`Eat | ${t('login')}`}</title>
+        <title>{`PickN\`Eat | ${t('profile')}`}</title>
       </Head>
       <Heading level={1} className={styles.profileTitle}>
-        [Profile]
+        {t('profile')}
       </Heading>
+
       {userPicture}
-      <p>nickname: {user.nickname}</p>
-      <p>name: {user.name}</p>
-      <Link title={t('logout')} href="/api/auth/logout">
-        <Button>{t('logout')}</Button>
+
+      {userInformation}
+
+      <Link
+        title={t2('logout')}
+        href="/api/auth/logout"
+        className={styles.logout}
+      >
+        <Button tabIndex={-1}>{t2('logout')}</Button>
       </Link>
     </MainContentLayout>
   );
 }
 
-Profile.messages = ['Navigation', ...PageLayout.messages];
+Profile.messages = ['Navigation', 'Profile', ...PageLayout.messages];
 
 export const getServerSideProps = async ({
   req,
