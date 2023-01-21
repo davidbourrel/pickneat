2741adf34441045b5555b0339b875d636e3d83dd
David <david-bourrel@hotmail.fr>
2023-01-21 07:36:29 +0100

    px to pourcent changed

--- hooks/useIntersectionObserver.tsx
+++ hooks/useIntersectionObserver.tsx
@@ -10,7 +10,7 @@ function useIntersectionObserver(
   {
     threshold = 0,
     root = null,
-    rootMargin = '0px 0px 0px 0px',
+    rootMargin = '0%',
     freezeOnceVisible = false,
   }: Args
 ): IntersectionObserverEntry | undefined {
