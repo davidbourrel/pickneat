501800cb6b9e4912f905350fe569c93e26928719
David <david-bourrel@hotmail.fr>
2023-04-06 20:48:01 +0200

    CategoryNavItem bg-color changed when its actived

--- components/modules/Navigation/CategoryNavigation/CategoryNavigationItem.tsx
+++ components/modules/Navigation/CategoryNavigation/CategoryNavigationItem.tsx
@@ -1,21 +1,24 @@
 import { CategoryNavigationItemProps } from './types';
+import styles from './CategoryNavigation.module.css';
 
 // Static Components
 import Button from 'components/elements/buttons/Button';
-import NavLink from 'components/elements/NavLink';
+import Link from 'components/elements/Link';
 
 const CategoryNavigationItem = ({
   id,
   isActive,
   children,
 }: CategoryNavigationItemProps) => {
+  const itemClassName = isActive ? styles.active : '';
+
   return (
-    <li>
-      <NavLink href={id} isActive={isActive}>
+    <li className={itemClassName}>
+      <Link href={id} className={styles.link}>
         <Button headless tabIndex={-1}>
           {children}
         </Button>
-      </NavLink>
+      </Link>
     </li>
   );
 };
